31a034e7ed1883559b15e87a3897247c7ab5b940
Lukasz <lukasz.koltun@op.pl>
2019-07-06 18:37:02 +0200

    console.log removed

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ btn.on('click', function () {
     if(counter===0){
         btn.html(`<i class="fas fa-angle-up"></i> Show less`);
         counter++;
-        console.log('dupa');
     }
     else{
         btn.html(`<i class="fas fa-angle-down"></i> Show more`);
